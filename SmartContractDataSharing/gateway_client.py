4ac7f7ad13d4436eec16e3d3925204d9752330f3
Andrew Martin <imchet.w@gmail.com>
2024-04-12 15:42:07 -0400

    a

--- SmartContractDataSharing/gateway_client.py
+++ SmartContractDataSharing/gateway_client.py
@@ -39,5 +39,5 @@ def download_from_gateway(file_hash):
 # Example usage of the client functions
 upload_response = upload_to_gateway('/practical/file.txt')  # Replace '/practical/file.txt' with your file path
 if upload_response:
-    file_hash = upload_response['Hash']
+    file_hash = upload_response['hash']
     download_from_gateway(file_hash)
