6edd99a3a50f209f99cd66fc8cd041883facaac2
Andrew Martin <imchet.w@gmail.com>
2024-04-12 18:48:55 -0400

    a

--- SmartContractDataSharing/eth_middleware.py
+++ SmartContractDataSharing/eth_middleware.py
@@ -13,7 +13,8 @@ class EthereumMiddleware:
         # Load the ABI from the contract JSON file
         with open('build/contracts/DataToken.json', 'r') as abi_file:
             self.contract_abi = json.load(abi_file)['abi']
-
+            print("Contract ABI:", self.contract_abi)  # Add this line to print the contract ABI
+        
         self.contract = self.web3.eth.contract(
             address=self.contract_address, 
             abi=self.contract_abi
